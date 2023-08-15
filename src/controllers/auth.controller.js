a9ab30f8526667880e050cee8bc63a31e2d3c4fa
ByJulioCundumi <julio.ce.dev@gmail.com>
2023-08-14 19:23:05 -0500

    auth routes, mongoose model and empty controller added

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import UserM from "../models/user.js"
+
+export async function postRegister(req, res){
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function postLogin(req, res){
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { postLogin, postRegister } from "../controllers/auth.controller.js";
+
+const router = Router()
+
+router.post("/register", postRegister)
+router.post("/login", postLogin)
