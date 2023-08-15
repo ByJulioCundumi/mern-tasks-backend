eaf0aa5b7dfb15ef066791d2669af4ccb2adfb2e
ByJulioCundumi <julio.ce.dev@gmail.com>
2023-08-14 21:00:16 -0500

    validate token middleware configured using cookie-parser library

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import express from "express"
 import cors from "cors"
 import morgan from "morgan" 
 import AuthRoutes from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
 
 const app = express()
 app.use(cors())
+app.use(cookieParser())
 app.use(express.json())
 app.use(morgan("dev"))
 //
 app.use("/api", AuthRoutes)
 //
-
 export default app;
