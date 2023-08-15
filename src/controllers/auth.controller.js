fb9d1488eff8b58c8bc38d9663eee23aada5c566
ByJulioCundumi <julio.ce.dev@gmail.com>
2023-08-14 20:23:37 -0500

    login endpoint ready and working correctly

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,8 +32,20 @@ export async function postRegister(req, res){
 }
 
 export async function postLogin(req, res){
+    const {email, password} = req.body;
     try {
-        
+        const found = await UserModel.findOne({email: email})
+        if(!found) return res.status(404).json({message: "User not found"})
+
+        const match = await bcrypt.compare(password, found.password)
+        if(!match) return res.status(400).json({message: "Invalid password"})
+
+        jwt.sign({id: found._id}, SECRET_KEY, {expiresIn: "1d"}, 
+        (err, token)=>{
+            if(err) console.log(err)
+            res.cookie("token", token)
+            res.json({message:"Loged Successfully"})
+        })
     } catch (error) {
         console.log(error)
     }
